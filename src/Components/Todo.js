788f1dd4412889efc37f67a507f3790a78bc6431
Kaviya <kaviya12198@gmail.com>
2021-10-07 09:16:47 +0530

    commit

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -5,33 +5,23 @@ import { TransitionGroup, CSSTransition } from "react-transition-group";
 const Todo = ({ toggleTodos, completed, task, id, deleteTodo }) => {
   return (
     <>
-    
-    <TransitionGroup className={completed ? "Todo" : "Todo.completed"}>
-      
-      <CSSTransition
-        className="card-header"
-        key="normal"
-        timeout={500}
-        
-      >
-        
-        <ul className="list-group">
-          
-        <li className="list-group-item"
-         onClick={toggleTodos}>
-          {task}
-          <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-          <button className= "btn btn-primary me-md-2 " onClick={deleteTodo}>
-          <i className="fas fa-trash" />
-          </button>
-          </div>
-        </li>
-        </ul>
-      </CSSTransition>
-      
-        
-      
-    </TransitionGroup>
+      <TransitionGroup className={completed ? "Todo" : "Todo.completed"}>
+        <CSSTransition className="card-header" key="normal" timeout={500}>
+          <ul className="list-group">
+            <li className="list-group-item" onClick={toggleTodos}>
+              {task}
+              <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                <button
+                  className="btn btn-primary me-md-2 "
+                  onClick={deleteTodo}
+                >
+                  <i className="fas fa-trash" />
+                </button>
+              </div>
+            </li>
+          </ul>
+        </CSSTransition>
+      </TransitionGroup>
     </>
   );
 };
